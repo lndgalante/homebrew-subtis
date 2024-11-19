e58fe3763d8c91e4bdfe1566731691d81aea5693
Leonardo Galante <lndgalante@gmail.com>
2024-11-18 22:04:38 -0300

    feat: Update sha256 to v0.4.5

--- Formula/subtis.rb
+++ Formula/subtis.rb
@@ -1,9 +1,9 @@
 class Subtis < Formula
   desc "Subtis CLI"
   homepage "https://github.com/lndgalante/homebrew-subtis"
-  url "https://github.com/lndgalante/homebrew-subtis/releases/download/v0.4.4/subtis"
-  sha256 "d3a2be488016ae0c5fed925a53244ebb82a2817f7b234596406d8ae84452f67b"
-  version "0.4.4"
+  url "https://github.com/lndgalante/homebrew-subtis/releases/download/v0.4.5/subtis"
+  sha256 "8f9e97d55925abbc819350fefb0ce165ed2766d3c7b8c64b34d502aa741e1694"
+  version "0.4.5"
 
   def install
     bin.install "subtis"

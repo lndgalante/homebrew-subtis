df838b3b8b9af310934f4ef2d4699c77e8d8d1bd
Leonardo Galante <lndgalante@gmail.com>
2024-12-31 23:31:30 -0300

    feat: Bump to v0.4.8

--- Formula/subtis.rb
+++ Formula/subtis.rb
@@ -1,9 +1,9 @@
 class Subtis < Formula
   desc "Subtis CLI"
   homepage "https://github.com/lndgalante/homebrew-subtis"
-  url "https://github.com/lndgalante/homebrew-subtis/releases/download/v0.4.7/subtis"
-  sha256 "b3784234d5a4dc6ddc33c3d29f2ac1b70b31c1a3c64e5f551a36ddc8b9fcb3ed"
-  version "0.4.7"
+  url "https://github.com/lndgalante/homebrew-subtis/releases/download/v0.4.8/subtis"
+  sha256 "0a7627b48f14a786173ec660e6662ea5e24700bf30ebc8ea2f8c47909e83c5dd"
+  version "0.4.8"
 
   def install
     bin.install "subtis"

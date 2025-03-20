f657c3c0ecafdbd7fe4a3efdb6bc14cb44f73061
Leonardo Galante <lndgalante@gmail.com>
2025-03-19 23:42:00 -0300

    feat: Bump CLI to v0.6.1

--- Formula/subtis.rb
+++ Formula/subtis.rb
@@ -1,9 +1,9 @@
 class Subtis < Formula
   desc "Subtis CLI"
   homepage "https://github.com/lndgalante/homebrew-subtis"
-  url "https://github.com/lndgalante/homebrew-subtis/releases/download/v0.5.8/subtis"
-  sha256 "37e768061abd2856501faea2753ee04fb3078d3251377a15d38600997eab10b4"
-  version "0.5.8"
+  url "https://github.com/lndgalante/homebrew-subtis/releases/download/v0.6.1/subtis"
+  sha256 "ae15c03fff812cf4bd0dc85a70605ae3bdba91d9dc712951e9dac9b82bd5a0c3"
+  version "0.6.1"
 
   def install
     bin.install "subtis"

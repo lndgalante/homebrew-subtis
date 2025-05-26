e667dd6c4ab19c1c8add7c4ec5dd53d0df397400
lndgalante <lndgalante@gmail.com>
2025-05-26 20:39:33 -0300

    feat: Bump Subtis to v0.7.5

--- Formula/subtis.rb
+++ Formula/subtis.rb
@@ -1,9 +1,9 @@
 class Subtis < Formula
   desc "Subtis CLI"
   homepage "https://github.com/lndgalante/homebrew-subtis"
-  url "https://github.com/lndgalante/homebrew-subtis/releases/download/v0.7.0/subtis"
-  sha256 "c206e9596f73a6ee0501516467a78218933c72dcbeba2af624f5a36001e03b77"
-  version "0.7.0"
+  url "https://github.com/lndgalante/homebrew-subtis/releases/download/v0.7.5/subtis"
+  sha256 "d20af2ae52f08f9fad0c5122f7ae629ba13cd52aa35f7ea6c4f97cc96c824c5f"
+  version "0.7.5"
 
   def install
     bin.install "subtis"

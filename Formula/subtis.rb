7f8537eeeb21ee62237a7b7dd3630503e849d562
Leonardo Galante <lndgalante@gmail.com>
2025-12-29 23:57:09 -0300

    Publish v0.9.0

--- Formula/subtis.rb
+++ Formula/subtis.rb
@@ -1,9 +1,9 @@
 class Subtis < Formula
   desc "Subtis CLI"
   homepage "https://github.com/lndgalante/homebrew-subtis"
-  url "https://github.com/lndgalante/homebrew-subtis/releases/download/v0.8.7/subtis"
-  sha256 "7877a6759f458303179657d128a014f7e9b18056328bee923974195f7e1ea588"
-  version "0.8.7"
+  url "https://github.com/lndgalante/homebrew-subtis/releases/download/v0.9.0/subtis"
+  sha256 "efe3d9dae5085b73ed7fd94bc1eb3e2bd4c68ac7678da177e3a944310895f3b2"
+  version "0.9.0"
 
   def install
     bin.install "subtis"
